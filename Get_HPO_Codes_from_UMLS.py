5256ba75fa4c850a4b79d4c207203f787cde5309
RichardNoll <158074324+RichardNoll@users.noreply.github.com>
2024-03-26 16:26:18 +0100

    Update Get_HPO_Codes_from_UMLS.py

--- Get_HPO_Codes_from_UMLS.py
+++ Get_HPO_Codes_from_UMLS.py
@@ -1,78 +1,114 @@
 import requests
 from urllib.parse import urlparse
 
-def get_last_part_of_url(url):
-    parsed_url = urlparse(url)
-    path_parts = parsed_url.path.split('/')
-    last_part = path_parts[-1]
-    return last_part
+class HealthTermFinder:
+    """
+    A tool for discovering standardized healthcare terminology across multiple languages using the UMLS API.
 
-def get_first_ui(apikey, version, string):
-    uri = "https://uts-ws.nlm.nih.gov"
-    content_endpoint = "/rest/search/"+version
-    full_url = uri + content_endpoint
-    page = 0
-    
-    try:
+    Attributes:
+        apikey (str): The API key required to access the UMLS API.
+        version (str): The version of the UMLS API to use. Defaults to 'current'.
+        session (requests.Session): A session object for making HTTP requests.
+
+    Methods:
+        make_request(endpoint, params): Makes an HTTP GET request to a specified UMLS API endpoint.
+        get_last_part_of_url(url): Extracts the last part of a URL's path.
+        get_first_ui(string): Searches for the first unique identifier (UI) based on a given search string.
+        get_atoms(identifier, source): Retrieves atom details for a given identifier, optionally filtering by source.
+    """
+
+    def __init__(self, apikey, version='current'):
+        """
+        Initializes the HealthTermFinder with the specified API key and version.
+
+        Parameters:
+            apikey (str): The API key for accessing the UMLS API.
+            version (str): The version of the UMLS API to use. Defaults to 'current'.
+        """
+        self.session = requests.Session()
+        self.apikey = apikey
+        self.version = version
+        self.base_uri = 'https://uts-ws.nlm.nih.gov'
+
+    def make_request(self, endpoint, params={}):
+        """
+        Makes an HTTP GET request to a specified endpoint of the UMLS API.
+
+        Parameters:
+            endpoint (str): The API endpoint to which the request is made.
+            params (dict): A dictionary of parameters to be passed with the request.
+
+        Returns:
+            dict: The JSON response from the API.
+
+        Raises:
+            HTTPError: If the request fails.
+        """
+        url = f"{self.base_uri}{endpoint}"
+        params['apiKey'] = self.apikey
+        response = self.session.get(url, params=params)
+        response.raise_for_status()
+        return response.json()
+
+    def get_last_part_of_url(self, url):
+        """
+        Extracts and returns the last part of a URL's path.
+
+        Parameters:
+            url (str): The URL from which to extract the last path segment.
+
+        Returns:
+            str: The last part of the URL's path.
+        """
+        return urlparse(url).path.split('/')[-1]
+
+    def get_first_ui(self, string):
+        """
+        Searches for and returns the first unique identifier (UI) for a given search string.
+
+        Parameters:
+            string (str): The search string used to query the UMLS API.
+
+        Returns:
+            str: The first unique identifier (UI) found, or None if no results are found.
+        """
+        content_endpoint = f"/rest/search/{self.version}"
+        page = 0
         while True:
             page += 1
-            query = {'string':string,'apiKey':apikey, 'pageNumber':page}
-            r = requests.get(full_url,params=query)
-            r.raise_for_status()
-            outputs  = r.json()
-        
-            items = outputs['result']['results']
-            
-            if len(items) == 0:
-                if page == 1:
-                    print('No results found.'+'\n')
-                    return None
-                else:
-                    break
-            
-            for result in items:
-                ui = result['ui']
-                return ui
-                
-    except Exception as except_error:
-        print(except_error)
-        return None
+            params = {'string': string, 'pageNumber': page}
+            results = self.make_request(content_endpoint, params)['result']['results']
+            if results:
+                return results[0]['ui']
+            if page == 1:
+                print('No results found.\n')
+                return None
 
-def get_atoms(apikey, version, identifier, source=None):
-    uri = 'https://uts-ws.nlm.nih.gov'
-    page = 0
+    def get_atoms(self, identifier, source=None):
+        """
+        Retrieves and prints details for atoms associated with a given identifier, optionally filtering by source.
 
-    try:
-        if source is None:
-            content_endpoint = '/rest/content/'+str(version)+'/CUI/'+str(identifier)+'/atoms'
+        Parameters:
+            identifier (str): The unique identifier for which to retrieve atom details.
+            source (str, optional): The source by which to filter the atoms. Defaults to None.
+        """
+        content_endpoint = f"/rest/content/{self.version}/CUI/{identifier}/atoms"
+        params = {'ttys': 'PT'}
+        if source:
+            params['sabs'] = source
         else:
-            content_endpoint = '/rest/content/'+str(version)+'/CUI/'+str(identifier)+'/atoms'
-                
-        query = {'apiKey':apikey, 'ttys': 'PT', 'sabs': 'HPO'}
-        r = requests.get(uri+content_endpoint, params=query)
-        r.encoding = 'utf-8'
-                
-        if r.status_code != 200:
-            raise Exception('Search term ' + "'" + str(identifier) + "'" + ' not found')
-                    
-        items  = r.json()
-                    
-        for atom in items['result']:
-            print('Name: ' + atom['name'])
-            print('Code: ' + get_last_part_of_url(atom['code']))
-            print('Source Vocabulary: ' + atom['rootSource'])
-            print('\n')
-            
-    except Exception as except_error:
-        print(except_error)
+            params['sabs'] = 'HPO,SNOMEDCT_US'
+        
+        atoms = self.make_request(content_endpoint, params)['result']
+        for atom in atoms:
+            print(f"Name: {atom['name']}")
+            print(f"Code: {self.get_last_part_of_url(atom['code'])}")
+            print(f"Source Vocabulary: {atom['rootSource']}\n")
 
 if __name__ == "__main__":
-    apikey = ""  # Set your API key here
-    version = "current"  # Set the version here
+    apikey = "" 
+    tool = HealthTermFinder(apikey)
     identifier = input("Enter identifier: ")
-    source = None
-
-
-    first_ui = get_first_ui(apikey, version, identifier)
+    first_ui = tool.get_first_ui(identifier)
     if first_ui:
-        get_atoms(apikey, version, first_ui, source)
+        tool.get_atoms(first_ui)
