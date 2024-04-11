26155a4bbff532e5d5e3513d94fdee351e449c04
RichardNoll <158074324+RichardNoll@users.noreply.github.com>
2024-04-11 17:10:30 +0200

    Update Get_Codes_from_UMLS.py

--- Get_Codes_from_UMLS.py
+++ Get_Codes_from_UMLS.py
@@ -48,17 +48,17 @@ class HealthTermFinder:
         
         params['apiKey'] = self.apikey
         try:
-            response = self.session.get(url, params=params)
-            
-            
-            response.raise_for_status()
-            return response.json()
+           response = self.session.get(url, params=params)
+           
+           
+           response.raise_for_status()
+           return response.json()
         except requests.HTTPError as e:
-            if e.response.status_code == 404:
-              return None
-            else:
-                print(f"HTTP Error occurred: {e}")
-            return None
+           if e.response.status_code == 404:
+             return None
+           else:
+               print(f"HTTP Error occurred: {e}")
+           return None
 
     def get_last_part_of_url(self, url):
         """
@@ -74,54 +74,42 @@ class HealthTermFinder:
 
     def get_first_ui(self, string):
         """
-        Searches for and returns the first 10 unique identifiers (UI) for a given search string.
+        Searches for and returns unique identifiers (UI) for a given search string.
+        It searches without source specification and then with 'MTH,MSH' sources.
     
         Parameters:
             string (str): The search string used to query the UMLS API.
     
         Returns:
-            list: The first three unique identifiers (UI) found, or an empty list if no results are found.
+            list: Unique identifiers (UI) found, up to 10 for each search type.
         """
         content_endpoint = f"/rest/search/{self.version}"
-        page = 0
         relevant_ui = []
     
-        while True:
-            page += 1
-            params = {'string': string, 'pageNumber': page, 'inputType': 'atom', 'sabs': '', 'partialSearch': 'true', 'searchType': 'words'}
+        # Search without specific source
+        params = {
+            'string': string,
+            'pageNumber': 1,
+            'inputType': 'atom',
+            'sabs': '',
+            'partialSearch': 'true',
+            'searchType': 'words'
+        }
+        response = self.make_request(content_endpoint, params)
+        results = response.get('result', {}).get('results', [])
+        relevant_ui.extend([result['ui'] for result in results[:10]])
+    
+        # Search with specific sources if the first search had results
+        if results:
+            params['sabs'] = 'MTH,MSH'
             response = self.make_request(content_endpoint, params)
             results = response.get('result', {}).get('results', [])
-             
-            
-            # Using a different sabs specification
-            params1 = {'string': identifier, 'pageNumber': 0, 'inputType': 'atom', 'sabs': 'MTH', 'partialSearch': 'true', 'searchType': 'words'}
-            response1 = self.make_request(f"/rest/search/{self.version}", params1)
-            results1 = response1.get('result', {}).get('results', [])
-        
+            relevant_ui.extend([result['ui'] for result in results[:10]])
     
-            if results:
-                
-                for result in results[:10]:  # Get up to the first 10 results
-                    relevant_ui.append(result['ui'])
-                           
-            
-            if results1:
-                
-                for result in results1[:10]:  # Get up to the first 10 results
-                    relevant_ui.append(result['ui'])
-                
-                break  # Exit the loop after collecting the necessary UIs
-    
-            
-            if page == 1 and not results:
-                print('No results found.')
-                break  # Exit the loop if no results on the first page
-
         return relevant_ui
     
 
-
-    def get_atoms(self, first_ui, source=None):
+    def get_atoms(self, first_ui):
         """
         Retrieves and prints details for atoms associated with a given identifier, optionally filtering by source.
 
@@ -133,25 +121,15 @@ class HealthTermFinder:
         for i in first_ui:
         
             content_endpoint = f"/rest/content/{self.version}/CUI/{i}/atoms"
-            params = {'ttys': 'PT'}
-            if source:
-                params['sabs'] = source
-            else:
-                params['sabs'] = 'HPO,SNOMEDCT_US'
-            
+            params = {'ttys': 'PT', 'sabs': 'HPO,SNOMEDCT_US'}
             atoms = self.make_request(content_endpoint, params)
-            
             if atoms:
                 for atom in atoms['result']:
                     print(f"Name: {atom['name']}")
                     print(f"Code: {self.get_last_part_of_url(atom['code'])}")
                     print(f"Source Vocabulary: {atom['rootSource']}\n")
                 break
-            
-           
-                
-    
-                 
+     
 
 if __name__ == "__main__":
     apikey = "" 
